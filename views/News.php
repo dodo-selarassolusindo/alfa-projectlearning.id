43b6cc5615a014bdad00a676d70b3b2dc0e7dd3d
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-01-22 15:20:32 +0700

    .

--- views/News.php
+++ views/News.php
@@ -15,7 +15,7 @@ $Page->showMessage();
     <div class="card-body">
 		<h6 class="card-title">22-01-2024 - Project Learning Membership <small>(alfa-release)</small> Released</h6>
 		<p class="card-text">For more information, please visit Project-Learning website</p>
-		<a href="https://projectlearning.id" class="btn btn-primary">Go to Project-Learning website</a>
+		<a target="_blank" href="https://projectlearning.id" class="btn btn-primary">Go to Project-Learning website</a>
 	</div>
 	<!--<div class="card-body">
 		<h6 class="card-title">2023/09/05 - PHPMaker 2024 Released</h6>
