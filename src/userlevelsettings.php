9d722dccabaa7a0bdc285b8f5ba46241961e8306
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-01-22 14:31:35 +0700

    .

--- src/userlevelsettings.php
+++ src/userlevelsettings.php
@@ -248,7 +248,7 @@ $USER_LEVEL_TABLES = [["cars","cars","Cars",true,"{DFB61542-7FFC-43AB-88E7-31D7F
     ["order details extended","order_details_extended","Order Details Extended",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}","orderdetailsextendedlist"],
     ["orders2","orders2","Orders 2",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}","orders2list"],
     ["cars2","cars2","Cars 2",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}","cars2list"],
-    ["home.php","home","Home Page",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}","home"],
+    ["home.php","home","Welcome",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}","home"],
     ["news.php","news","News",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}","news"],
     ["dji","dji","Dow Jones Index",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}",""],
     ["employee_sales_by_country_for_2014","employee_sales_by_country_for_2014","employee sales by county for 2014",true,"{DFB61542-7FFC-43AB-88E7-31D7F8D95066}",""],
