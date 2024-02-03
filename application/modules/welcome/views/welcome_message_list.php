8f2aebd218096cf0893caa15b911cbd1837217a7
dodo <dodo@dodo.dodo>
2024-02-03 19:19:30 +0700

    .

--- application/modules/welcome/views/welcome_message_list.php
+++ application/modules/welcome/views/welcome_message_list.php
@@ -5,12 +5,8 @@
             <h1 class="text-body-emphasis">Project Learning</h1>
             <p class="col-lg-8 mx-auto lead">
                 Selamat datang di Project Learning, pembelajaran proyek konstruksi,
-                <!-- This takes the basic jumbotron above and makes its background edge-to-edge with a <code>.container</code> inside to align content. Similar to above, it's been recreated with built-in grid and utility classes. -->
-            <!-- </p>
-            <p class="col-lg-8 mx-auto lead"> -->
                 <br>
                 dapatkan berbagai materi pembelajaran terbaik di website ini
-                <!-- This takes the basic jumbotron above and makes its background edge-to-edge with a <code>.container</code> inside to align content. Similar to above, it's been recreated with built-in grid and utility classes. -->
             </p>
         </div>
     </div>
